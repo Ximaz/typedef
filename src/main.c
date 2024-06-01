536c88a4767dbb8ce7710ca171200c5b14bbca16
DURAND Malo <malo.durand@epitech.eu>
2024-06-01 13:16:40 +0200

    init: reading file in main

--- src/main.c
+++ src/main.c
@@ -6,7 +6,9 @@
 */
 
 #include <stdio.h>
+#include <stdlib.h>
 #include "args_parser.h"
+#include "file.h"
 
 #ifndef UNIT_TESTS
 
@@ -20,12 +22,19 @@ static void usage(const char *program)
 
 int main(int argc, const char *argv[])
 {
+    char *jsdoc_src = NULL;
     args_t args = { 0 };
 
     if (-1 == args_parser(&args, argc, argv)) {
         usage(*argv);
         return 0;
     }
+    jsdoc_src = file_read(args.jsdoc_src_path);
+    if (NULL == jsdoc_src) {
+        printf("Unable to read the JSDoc file at '%s'.", args.jsdoc_src_path);
+        return 0;
+    }
+    free(jsdoc_src);
     return 0;
 }
 
